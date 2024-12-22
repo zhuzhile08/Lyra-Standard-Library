2297fc35a1be5c006be29100bb53cedae450eb0c
Zhile Zhu <zhuzhile08@gmail.com>
2024-12-22 16:16:10 +0100

    More advanced formatter testing

--- Tests/Format/main.cpp
+++ Tests/Format/main.cpp
@@ -2,6 +2,7 @@
 #include <format>
 
 int main() {
-	std::printf("%s\n", lsd::format("Ha{:f<5}a\nLo{0:l^10}", 'h', 'l').data());
-	std::print("{},\n{:#},\n{},\n{}\n", true, false, true, false);
+	std::printf("%s\n", lsd::format("Ha{:a<5}\nLo{0:l^10}\n{2}\n", 'h', 'l', 12345.34567897f).data());
+	std::printf("%s\n", lsd::format("Hello {}\n", "world").data());
+	//std::print("{},\n{:A},\n{},\n{}\n", true, 0.0f, true, false);
 }
